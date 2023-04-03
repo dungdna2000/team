291174b0c0aba79ab48cf8857d1168c94e8dbca2
Dinh Nguyen Anh Dung <dungdna2000@gmail.com>
2023-04-03 14:59:35 +0700

    Dung implemented part of Rectangle class

---
+++ rec.cpp
@@ -0,0 +1,3 @@
+#include "rec.h"
+
+// I AM TOO LAZY TO IMPLEMNT IT ! HE EHE EHE EHE 
--- rec.h
+++ rec.h
@@ -1,5 +1,11 @@
 #pragma once
 class Rectangle
 {
-	
+	float width, height;
+public: 
+	Rectangle(float w, float h)
+	{
+		width = w;
+		height = h;
+	}
 }
