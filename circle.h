14127b91165213fd7c0db3cda555e06af6998d03
Bob The Tester <aeonend.tester@gmail.com>
2023-04-03 14:52:27 +0700

    Bob implement class Circle

--- circle.h
+++ circle.h
@@ -1,5 +1,9 @@
 #pragma once
 class Circle
 {
-
+	float radius;
+public: 
+	Circle(int r){
+		radius = r;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "circle.h"
 using namespace std;
+
+void test_circle()
+{
+	Circle c(5);
+	c.print();
+}
+
 int main(int)
 {
 	cout << "Hello, world. From DungDNA and from BOB. We are good devs!!" << endl;
